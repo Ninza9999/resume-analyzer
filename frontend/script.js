6bd514d230329e9eeaa516a44552ce1af5541b28
Nikhil Dalal <nikhildalal@Nikhils-MacBook-Air.local>
2026-05-03 17:01:49 +0530

    final UI + backend

--- frontend/script.js
+++ frontend/script.js
@@ -22,7 +22,7 @@ async function uploadFile() {
     formData.append("resume", file);
 
     try {
-        const response = await fetch("https://YOUR-RENDER-URL.onrender.com/analyze", {
+        const response = await fetch("https://resume-analyzer-ehx2.onrender.com/analyze", {
             method: "POST",
             body: formData
         });
